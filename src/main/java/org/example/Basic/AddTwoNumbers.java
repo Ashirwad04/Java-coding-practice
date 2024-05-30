1bac998eb417e2bb7db3a8da579e1908d762aaad
Ashirwad04 <ashirwadk04@gmail.com>
2024-05-30 18:23:39 +0530

    Second Commit

---
+++ src/main/java/org/example/Basic/AddTwoNumbers.java
@@ -0,0 +1,15 @@
+package org.example.Basic;
+
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        // Declare two variables to hold the numbers
+        int num1 = 5;
+        int num2 = 10;
+
+        // Add the two numbers
+        int sum = num1 + num2;
+
+        // Print the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum);
+    }
+}
